8b284a1529a6182e271fac9b992a2509fec965e2
Zainab siddiq <zainabsiddiq2308@gmail.com>
2025-10-29 16:50:17 +0500

    updated

--- src/app/abc/page.js
+++ src/app/abc/page.js
@@ -1,8 +1,7 @@
 import React from 'react'
-import { useRouter } from 'next/navigation'
+
 const Abc = () => {
-    const route=useRouter()
-    route.push("/forms/abc")
+  
   return (
     <div>page</div>
   )
