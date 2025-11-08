7e1ce7ef63c35deef33009b34113c65250f92eb3
Zainab siddiq <zainabsiddiq2308@gmail.com>
2025-11-08 16:13:33 +0500

    updated

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,19 @@
+// created at 8 novermber
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://www.aapkiwebsite.com', // 👈 apni live domain daalo
-  generateRobotsTxt: true, // (optional) robots.txt bhi banayega
-  sitemapSize: 7000,
+  siteUrl: 'https://techwizpk.com',
+  generateRobotsTxt: true,
+  transform: async (config, path) => {
+    // Only include the homepage
+    if (path === '/') {
+      return {
+        loc: `${config.siteUrl}${path}`, // full URL
+        lastmod: '2025-11-08T08:42:56+00:00',
+        changefreq: 'daily',
+        priority: 1.0,
+      };
+    }
+    // Skip all other paths
+    return undefined;
+  },
 };
