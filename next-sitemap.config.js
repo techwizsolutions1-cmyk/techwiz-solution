fc31be09d3277e6d3a267ae94ddeff221a198596
Zainab siddiq <zainabsiddiq2308@gmail.com>
2025-11-08 16:03:38 +0500

    upday

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.aapkiwebsite.com', // 👈 apni live domain daalo
+  generateRobotsTxt: true, // (optional) robots.txt bhi banayega
+  sitemapSize: 7000,
+};
