fdfe423aa3a48520de9e7f4a590e18dec82e6c42
Zainab siddiq <zainabsiddiq2308@gmail.com>
2025-11-08 16:33:23 +0500

    updated

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,19 +1,17 @@
-// created at 8 novermber
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://techwizpk.com',
   generateRobotsTxt: true,
+  generateIndexSitemap: true, // ← yeh line add karo
   transform: async (config, path) => {
-    // Only include the homepage
     if (path === '/') {
       return {
-        loc: `${config.siteUrl}${path}`, // full URL
+        loc: `${config.siteUrl}${path}`,
         lastmod: '2025-11-08T08:42:56+00:00',
         changefreq: 'daily',
         priority: 1.0,
       };
     }
-    // Skip all other paths
     return undefined;
   },
 };
